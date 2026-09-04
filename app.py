ca9bbde19d9e2a0b5bac8bd0e24ef54d3fb88930
Neo <neorapza231@gmail.com>
2026-09-04 19:12:21 +0200

    added Student registration

--- app.py
+++ app.py
@@ -14,3 +14,26 @@ def init_db_command():
     print("Initialised the SQLite database.")
 
 # 1. Student Regiastration
+@app.route("api/register", methods=['POST'])
+def register():
+    data = request.get_json() or {}
+    required = ['student_number', 'full_name', 'password', 'residence_address']
+    if not all(field in data for field in required):
+        return jsonify({"error": "Missing required fields"}), 400
+
+    hashed_pw = generate_password_hash(data["password"])
+    db = get_db()
+
+    try:
+        cursor = db.execute(
+            """
+            INSERT INTO students (student_number, full_name, password_hash, phone, residence_address)
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (data["student_number"], data["full_name"], hashed_pw, data["phone"], data["residence_address"])
+        )
+        db.commit()
+    except db.IntegrityError:
+        return jsonify({'error': "Student number already registered"}),400
+
+    return jsonify({'message': 'Student registered successfully', "student_id": cursor.lastrowid}), 201
